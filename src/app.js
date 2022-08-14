c416262d32647a67acc65f9445e414ea3ea9bf73
arjuntp <apuliyullaveetil@viralnation.com>
2022-08-14 01:13:04 -0400

    package json

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ passport.use('jwt', jwtStrategy);
 
 
 // v1 api routes
-app.use('/v1', routes);
+app.use('/', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
