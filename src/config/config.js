48230040cb875ba24706d16ba375ecc6de7cc675
arjuntp <apuliyullaveetil@viralnation.com>
2022-08-14 00:28:23 -0400

    added port

--- src/config/config.js
+++ src/config/config.js
@@ -34,7 +34,7 @@ if (error) {
 
 module.exports = {
   env: envVars.NODE_ENV,
-  port: envVars.PORT,
+  port: envVars.PORT || 3001,
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
     options: {
